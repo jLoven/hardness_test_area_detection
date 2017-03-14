8eb4907a7468026ca44dcdd45f734b813091243e
Jackie <jackieloven@gmail.com>
2017-03-14 18:20:16 -0400

    adding generated images folder

--- auto_canny.py
+++ auto_canny.py
@@ -1,3 +1,8 @@
+# Jackie Loven
+# 6 March 2017
+# Adapted from pyimagesearch.com
+
+
 # USAGE
 # python auto_canny.py --images images
 
@@ -21,24 +26,28 @@ def auto_canny(image, sigma=0.33):
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--images", required=True,
-	help="path to input dataset of images")
+ap.add_argument("-i", "--image", required=True,
+	help="path to image")
+ap.add_argument("--filename", required = True)
 args = vars(ap.parse_args())
 
 # loop over the images
-for imagePath in glob.glob(args["images"] + "/*.jpg"):
-	# load the image, convert it to grayscale, and blur it slightly
-	image = cv2.imread(imagePath)
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+# for imagePath in glob.glob(args["images"] + "/*.png"):
+# load the image, convert it to grayscale, and blur it slightly
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (3, 3), 0)
 
-	# apply Canny edge detection using a wide threshold, tight
-	# threshold, and automatically determined threshold
-	wide = cv2.Canny(blurred, 10, 200)
-	tight = cv2.Canny(blurred, 225, 250)
-	auto = auto_canny(blurred)
+# apply Canny edge detection using a wide threshold, tight
+# threshold, and automatically determined threshold
+wide = cv2.Canny(blurred, 10, 200)
+#tight = cv2.Canny(blurred, 225, 250)
+#auto = auto_canny(blurred)
 
-	# show the images
-	cv2.imshow("Original", image)
-	cv2.imshow("Edges", np.hstack([wide, tight, auto]))
-	cv2.waitKey(0)
+# write the new image
+cv2.imwrite("generated_images/" + args["filename"] + ".png", wide)
+
+# show the images
+cv2.imshow("Original", image)
+cv2.imshow("Edges", wide)
+cv2.waitKey(0)	
