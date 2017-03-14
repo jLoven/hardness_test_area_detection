3eb3add18c94530038f41ca3725e2a589d672c0d
Jacquelyn Loven <platypus@dhcp-clara-651.eduroam.cornell.edu>
2017-03-14 17:56:54 -0400

    trying out image histograms

--- histogram.py
+++ histogram.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('wiki.jpg',0)
+img = cv2.imread('images/5.png',0)
 
 hist,bins = np.histogram(img.flatten(),256,[0,256])
 
