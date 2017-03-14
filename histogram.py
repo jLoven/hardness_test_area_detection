55d2516b45f49086d132db2d3a0d2426cee13ab4
Jacquelyn Loven <platypus@dhcp-clara-651.eduroam.cornell.edu>
2017-03-14 18:05:45 -0400

    normalizing image and showing histogram

--- histogram.py
+++ histogram.py
@@ -4,8 +4,8 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('images/5.png',0)
 
-hist,bins = np.histogram(img.flatten(),256,[0,256])
-
+#  Grab a histogram of original image
+hist, bins = np.histogram(img.flatten(),256,[0,256])
 cdf = hist.cumsum()
 cdf_normalized = cdf * hist.max()/ cdf.max()
 
@@ -14,3 +14,26 @@ plt.hist(img.flatten(),256,[0,256], color = 'r')
 plt.xlim([0,256])
 plt.legend(('cdf','histogram'), loc = 'upper left')
 plt.show()
+
+# Equalize original image
+cdf_m = np.ma.masked_equal(cdf,0)
+cdf_m = (cdf_m - cdf_m.min())*255/(cdf_m.max()-cdf_m.min())
+cdf = np.ma.filled(cdf_m,0).astype('uint8')
+img2 = cdf[img]
+
+# Show the updated histogram
+hist2, bins2 = np.histogram(img2.flatten(),256,[0,256])
+cdf2 = hist2.cumsum()
+cdf_normalized2 = cdf2 * hist2.max()/ cdf2.max()
+
+plt.plot(cdf_normalized2, color = 'b')
+plt.hist(img2.flatten(),256,[0,256], color = 'r')
+plt.xlim([0,256])
+plt.legend(('cdf2','histogram2'), loc = 'upper left')
+plt.show()
+
+
+
+
+
+
