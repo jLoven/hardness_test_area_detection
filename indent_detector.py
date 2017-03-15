0db903bd9cf927eeb97b0840ce507eb3217a821b
Jacquelyn Loven <platypus@dhcp-clara-651.eduroam.cornell.edu>
2017-03-14 20:22:55 -0400

    finished odell's list

--- indent_detector.py
+++ indent_detector.py
@@ -13,7 +13,7 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 
 # 1. Convert to grayscale
-grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+grayImage = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2GRAY)
 
 # 2. Simple Gaussian blur
 # window size 7 x 7, exact
@@ -37,13 +37,43 @@ erodeImage3 = cv2.erode(claheImage, kernel, iterations = 1)
 dilateImage2 = cv2.dilate(erodeImage3, kernel, iterations = 1)
 
 # 6. Adaptive Gaussian thresholding
+adaptiveGaussianThresholdImage = cv2.adaptiveThreshold(dilateImage2, 
+	255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
+# 7. Close, open
+dilateImage3 = cv2.dilate(adaptiveGaussianThresholdImage, kernel, 
+	iterations = 1)
+erodeImage4 = cv2.erode(dilateImage3, kernel, iterations = 1)
 
+# 8. Canny edge detection
+# Following function is from https://goo.gl/E9F4m8 :
+def auto_canny(image, sigma = 0.33):
+	# compute the median of the single channel pixel intensities
+	v = np.median(image)
+ 
+	# apply automatic Canny edge detection using the computed median
+	lower = int(max(0, (1.0 - sigma) * v))
+	upper = int(min(255, (1.0 + sigma) * v))
+	edged = cv2.Canny(image, lower, upper)
+ 
+	# return the edged image
+	return edged
 
+cannyImage = auto_canny(erodeImage4)
 
+# 9. Gaussian blur
+gaussianBlurImage2 = cv2.GaussianBlur(cannyImage, (7, 7), 0)
 
+# 10. Contour search using perimeter and area
+contours = cv2.findContours(gaussianBlurImage2, 
+	cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+(cnts, _) = contours.sort_contours(cnts)
+for c in cnts:
+	if cv2.contourArea(c) < 50:
+		continue
+	cv2.drawContours(image, [c], -1, (255, 192, 203), 3)
 
-
-
+cv2.imshow("Image", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
