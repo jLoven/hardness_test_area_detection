708528cfec4eeec474fc235899246249e72e627e
Jackie <Jackie@platypus.icsd.k12.ny.us>
2017-04-26 16:25:55 -0400

    Tryna get the scale bar

--- color_detector.py
+++ color_detector.py
@@ -43,6 +43,8 @@ display(imageCopy)
 #RGB
 RED_MIN = np.array([40, 40, 200], np.uint8)
 RED_MAX = np.array([120, 120, 250], np.uint8)
+GREEN_MIN = np.array([175, 75, 90], np.uint8)
+GREEN_MAX = np.array([195, 130, 140], np.uint8)
 
 #display(imageCopy)
 dst = cv2.inRange(imageCopy, RED_MIN, RED_MAX)
@@ -50,29 +52,48 @@ no_red = cv2.countNonZero(dst)
 invertImage = cv2.bitwise_not(dst)
 #display(invertImage)
 
+dst2 = cv2.inRange(imageCopy, GREEN_MIN, GREEN_MAX)
+no_green = cv2.countNonZero(dst2)
+invertImage2 = cv2.bitwise_not(dst2)
+
 kernel = np.ones((7, 7), np.uint8)
 erodeImage1 = cv2.erode(invertImage, kernel, iterations = 1)
+erodeImage2 = cv2.erode(invertImage2, kernel, iterations = 1)
 #display(erodeImage1, "erode1")
 dilateImage1 = cv2.dilate(erodeImage1, kernel, iterations = 1)
+dilateImage2 = cv2.dilate(erodeImage2, kernel, iterations = 1)
 display(dilateImage1, "dilate1")
+display(dilateImage2, "dilate2")
 
 cannyImage = auto_canny(dilateImage1)
+cannyImage2 = auto_canny(dilateImage2)
 blurredImage1 = cv2.GaussianBlur(cannyImage, (3, 3), 0)
+blurredImage2 = cv2.GaussianBlur(cannyImage2, (3, 3), 0)
 #display(blurredImage1)
 
 
 # Find contours:
 cnts = cv2.findContours(blurredImage1, 
 	cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+cnts2 = cv2.findContours(blurredImage2, 
+	cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+cnts2 = cnts2[0] if imutils.is_cv2() else cnts2[1]
 (cnts, _) = contours.sort_contours(cnts)
+(cnts2, _) = contours.sort_contours(cnts2)
 relevantContour = cnts[0]
+relevantContour2 = cnts2[0]
 cv2.drawContours(imageCopy, [relevantContour], 0, (0, 255, 127), 2)
+cv2.drawContours(imageCopy, [relevantContour2], 0, (0, 255, 127), 2)
 
 rect = cv2.minAreaRect(relevantContour)
+rect2 = cv2.minAreaRect(relevantContour2)
 box = cv2.cv.BoxPoints(rect)
+box2 = cv2.cv.BoxPoints(rect2)
 box = np.int0(box)
+box2 = np.int0(box2)
 cv2.drawContours(imageCopy,[box],0,(255, 0, 0),2)
+cv2.drawContours(imageCopy,[box2],0,(255, 255, 0),2)
 
 #print('Indent area is ' + cv2.contourArea(cnts[0]))
 display(imageCopy)
