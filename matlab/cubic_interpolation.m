fbf41ac05a5dd967b3bd538079aaaba6ddf09836
Jacquelyn Loven <platypus@dhcp-hol-4445.eduroam.cornell.edu>
2017-06-21 18:30:45 -0400

    new data

--- matlab/cubic_interpolation.m
+++ matlab/cubic_interpolation.m
@@ -1,16 +1,28 @@
 
+xLength = 5;
+yLength = 5;
+sampleIndent = [ 0 0 0 0 0;
+  0 -1 -1 -1 0;
+  0 -1 -2 -1 0;
+  0 -1 -1 -1 0;
+  0 0 0 0 0
+];
 
 
-x = -3 + 6 * rand(50, 1);
-y = -3 + 6 * rand(50, 1);
-v = zeros(50, 1);
+[X,Y]=ndgrid(1:sampleIndent, 1:yLength);
+f = fit([X(:),Y(:)], sampleIndent(:),'linearinterp');
+plot( f, [X(:),Y(:)], sampleIndent(:))
 
-[xq,yq] = meshgrid(-3:0.1:3);
+%x = -3 + 6 * rand(50, 1);
+%y = -3 + 6 * rand(50, 1);
+%v = zeros(100, 1);
 
-z4 = griddata(x,y,v,xq,yq,'cubic');
-figure
-plot3(x,y,v,'mo')
-hold on
-mesh(xq,yq,z4)
-title('Cubic')
-legend('Sample Points','Interpolated Surface','Location','NorthWest')
+% [xq,yq] = meshgrid(-3:0.2:3);
+% 
+% z4 = griddata(x,y,v,xq,yq,'cubic');
+% figure
+% plot3(x,y,v,'mo')
+% hold on
+% mesh(xq,yq,z4)
+% title('Cubic')
+% legend('Sample Points','Interpolated Surface','Location','NorthWest')
