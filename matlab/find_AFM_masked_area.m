f4344ba088295ab4bd2e10d30eee864609c3d857
Jacquelyn Loven <platypus@dhcp-hol-115.eduroam.cornell.edu>
2017-07-26 14:42:23 -0400

    notes, afm anaylsis

--- matlab/find_AFM_masked_area.m
+++ matlab/find_AFM_masked_area.m
@@ -2,14 +2,15 @@
 %  Have the user click several points on the scale bar and input how far apart they are,
 %  take an average.
 
-original = imread('/Users/platypus/Desktop/mse_4920/hardness_test_area_detection/data/afm/largest_10004_mask.jpg');
+original = imread('/Users/platypus/Desktop/mse_4920/hardness_test_area_detection/data/afm/mask_smallest_4.jpg');
+%original = imread('/Users/platypus/Desktop/mask_largest_1.jpg');
 originalCopy = original;
 secondCopy = original;
 [width, height, z1] = size(originalCopy);
 
-figure('name','Please click four consecutive points on the scale bar.');
+figure('name','Please click five consecutive points on the vertical scale bar.');
 imshow(originalCopy);
-[x, y] = ginput(4);
+[x, y] = ginput(5);
 
 prompt = {'Scale bar size in µm:'};
 dlg_title = 'Input';
@@ -23,14 +24,15 @@ close all;
 difference1 = abs(y(3) - y(2));
 difference2 = abs(y(2) - y(1));
 difference3 = abs(y(4) - y(3));
-average = (difference1 + difference2 + difference3) / 3;
+difference4 = abs(y(5) - y(4));
+average = (difference1 + difference2 + difference3 + difference4) / 4;
 
 micronsPerPixel = scaleBarSize / average;
 
 rpcount = 0;
 for i = 1:width
     for j = 1:height
-        first = originalCopy(i, j, 1) <= 255 && originalCopy(i, j, 1) >= 190;
+        first = originalCopy(i, j, 1) <= 255 && originalCopy(i, j, 1) >= 110;
         second = originalCopy(i, j, 2) <= 100 && originalCopy(i, j, 2) >= 0;
         third = originalCopy(i, j, 3) <= 100 && originalCopy(i, j, 3) >= 0;
         if first && second && third
@@ -44,4 +46,5 @@ end
 
 imshow(secondCopy);
 areaOfRed = rpcount * micronsPerPixel * micronsPerPixel;
-disp(areaOfRed);
+totalString = strcat(num2str(areaOfRed), ' sq. µm');
+disp(totalString);
