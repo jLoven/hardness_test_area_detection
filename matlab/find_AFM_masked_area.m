b76dbaada4571dec86e72b754925811c65d0f4f0
Jacquelyn Loven <platypus@dhcp-hol-4662.eduroam.cornell.edu>
2017-08-15 12:25:13 -0400

    new matlab for indent corners

--- matlab/find_AFM_masked_area.m
+++ matlab/find_AFM_masked_area.m
@@ -4,7 +4,7 @@
 % Mask indent area in an AFM image in red, and click several points on the 
 % scale bar to identify pixels per area.
 
-original = imread('/Users/platypus/Desktop/mse_4920/images/afm/redone_masks_2/2942_4_edit.png');
+original = imread('/Users/platypus/Desktop/mse_4920/images/afm/redone_masks_2/245.2_4.png');
 originalCopy = original;
 secondCopy = original;
 [width, height, z1] = size(originalCopy);
@@ -56,20 +56,66 @@ binaryImage = imbinarize(cannyImageCast,'adaptive','ForegroundPolarity','dark','
 im = binaryImage;
 im_fill = imfill(im, 'holes');
 s = regionprops(im_fill, 'Area', 'PixelList');
-[~,ind] = max([s.Area]);
-pix = sub2ind(size(im), s(ind).PixelList(:,2), s(ind).PixelList(:,1));
-pixelArea = s(ind).Area;
+[~,index] = max([s.Area]);
+pixelArea = s(index).Area;
+relevantPixelList = s(index).PixelList;
 
 % Color the indent green on the image.
 for i = 1:pixelArea
-    x = s(ind).PixelList(i, 2);
-    y = s(ind).PixelList(i, 1);
+    x = relevantPixelList(i, 2);
+    y = relevantPixelList(i, 1);
     originalCopy(x, y, 1) = 0;
     originalCopy(x, y, 2) = 255;
     originalCopy(x, y, 3) = 0;
 end
 
-imshow(originalCopy);
-areaOfRed = pixelArea * micronsPerPixel * micronsPerPixel;
-totalString = strcat(num2str(areaOfRed), ' sq. µm');
+areaOfIndent = pixelArea * micronsPerPixel * micronsPerPixel;
+totalString = strcat(num2str(areaOfIndent), ' sq. µm');
 disp(totalString);
+
+figure('name','Please click the four indent vertices in clockwise order.');
+imshow(originalCopy);
+
+%  Following taken from https://www.mathworks.com/matlabcentral/answers/95813-how-can-i-use-the-ginput-function-to-select-a-point-on-an-image-in-matlab-along-with-the-zoom-functi
+zoom on;
+pause() % you can zoom with your mouse and when your image is okay, you press any key
+zoom off; % to escape the zoom mode
+point1 = ginput(1);
+zoom out; % go to the original size of your image
+zoom on;
+pause() % you can zoom with your mouse and when your image is okay, you press any key
+zoom off; % to escape the zoom mode
+point2 = ginput(1);
+zoom out; % go to the original size of your image
+zoom on;
+pause() % you can zoom with your mouse and when your image is okay, you press any key
+zoom off; % to escape the zoom mode
+point3 = ginput(1);
+zoom out; % go to the original size of your image
+zoom on;
+pause() % you can zoom with your mouse and when your image is okay, you press any key
+zoom off; % to escape the zoom mode
+point4 = ginput(1);
+zoom out; % go to the original size of your image
+
+% sqrt((x2-x1)^2+(y2-y1)^2)
+diagonal1 = sqrt((point1(1) - point3(1))^2 + ((point1(2) - point3(2))^2));
+diagonal2 = sqrt((point2(1) - point4(1))^2 + ((point2(2) - point4(2))^2));
+% draw diagonals in blue
+hold on;
+plot([point1(1),point3(1)],[point1(2),point3(2)],'Color','blue','LineWidth',2)
+plot([point2(1),point4(1)],[point2(2),point4(2)],'Color','blue','LineWidth',2)
+
+avgDiag = (diagonal1 + diagonal2) / 2;
+avgDiagInMicrons = avgDiag * micronsPerPixel;
+diagonalArea = avgDiagInMicrons^2 * 0.5;
+totalString2 = strcat(num2str(diagonalArea), ' sq. µm for diagonals');
+disp(totalString2);
+
+
+
+% Calculate diagonals 
+
+
+
+
